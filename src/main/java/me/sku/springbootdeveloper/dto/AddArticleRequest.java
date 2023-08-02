f547217cee17d9b2623cd4618728e7550d3086e2
ksks723 <ksks723@gmail.com>
2023-08-03 00:26:51 +0900

    postMapping

---
+++ src/main/java/me/sku/springbootdeveloper/dto/AddArticleRequest.java
@@ -0,0 +1,18 @@
+package me.sku.springbootdeveloper.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import me.sku.springbootdeveloper.domain.Article;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+public class AddArticleRequest {
+    private String title;
+    private String content;
+
+    public Article toEntity(){//빌더 패턴을 사용해 DTO를 엔티티로 만들어주는 메서드다. 블로그 글을 추가할 때 저장할 엔티티로 변환하는 용도이다.
+        return Article.builder().title(title).content(content).build();
+    }
+}
